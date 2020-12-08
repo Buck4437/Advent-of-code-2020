b1900ad32b29f2e46b026ab4fc9c54331206013a
Buck4437 <Buck4437@users.noreply.github.com>
2020-12-08 15:57:43 +0800

    Refactor
    Currently Q1 Q2 done

---
+++ src/com/company/Question01/Main.java
@@ -0,0 +1,45 @@
+package com.company.Question01;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("src\\com\\company\\Question01\\input.txt");
+        Scanner sc = new Scanner(file);
+
+        ArrayList<Integer> listOfNum = new ArrayList<>();
+        while (sc.hasNextLine()){
+            int num = Integer.parseInt(sc.nextLine());
+            listOfNum.add(num);
+        }
+
+        //part 1
+        for (int num : listOfNum){
+            int num2 = 2020 - num;
+            if (listOfNum.contains(num2)){
+                System.out.printf("Number pair: %s and %s. Total: %s\n", num, num2, num * num2);
+                break;
+            }
+        }
+
+        //part 2
+        boolean numFound = false;
+        for (int num1 : listOfNum){
+            for (int num2 : listOfNum) {
+                int num3 = 2020 - num1 - num2;
+                if (listOfNum.contains(num3)){
+                    System.out.printf("Number pair: %s, %s and %s. Total: %s\n", num1, num2, num3, num1 * num2 * num3);
+                    numFound = true;
+                    break;
+                }
+            }
+            if (numFound){
+                break;
+            }
+        }
+    }
+
+}
--- src/com/company/Question02/Password.java
+++ src/com/company/Question02/Password.java
@@ -1,12 +1,12 @@
-package com.company;
+package com.company.Question02;
 
-public class Question02Password {
+public class Password {
 
     char targetChar;
     String password;
     int numArg1, numArg2;
 
-    public Question02Password(String policies){
+    public Password(String policies) {
         String[] policiesArray = policies.split(" ");
 
         targetChar = policiesArray[1].charAt(0);
@@ -18,9 +18,10 @@ public class Question02Password {
         numArg2 = Integer.parseInt(numberArgsStr[1]);
     }
 
-    public boolean isValid(int checkType){
+    public boolean isValid(int checkType) {
         switch (checkType){
             case 1:
+                //part 1
                 int occurrence = 0;
                 for (char pwdChar : password.toCharArray()){
                     if (pwdChar == targetChar) {
@@ -30,6 +31,7 @@ public class Question02Password {
                 return occurrence >= numArg1 && occurrence <= numArg2;
 
             case 2:
+                //part 2
                 char[] pwdCharArray = password.toCharArray();
                 return pwdCharArray[numArg1 - 1] == targetChar ^ pwdCharArray[numArg2 - 1] == targetChar;
 
